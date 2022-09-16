7b07043aff0d1268ef00ecdba0effe145bbd4581
ft <1610581330@qq.com>
2022-09-16 17:55:30 +0800

    no message

---
+++ test10.py
@@ -0,0 +1,54 @@
+#  10 个时尚类别的 60,000 个 28x28 灰度图像的数据集，以及一个包含 10,000 个图像的测试集
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
+import matplotlib.pyplot as plt
+
+
+#[60k,28,28,3]  [60k]
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.boston_housing.load_data()
+# y_test = tf.squeeze(y_test)
+# y_train = tf.squeeze(y_train)
+# y_test = tf.one_hot(y_test, depth=10) # [10k, 10]
+# y_train = tf.one_hot(y_train, depth=10)
+
+print('datasets:', x_train.shape, y_train.shape, x_test.shape, y_test.shape, tf.reduce_max(x_train), tf.reduce_min(x_train))
+
+model = tf.keras.applications.Xception(
+    include_top=True,
+    weights=None,
+    input_tensor=None,
+    input_shape=(400, 400, 3),
+    pooling=None,
+    classes=5,
+    classifier_activation='softmax')
+
+model.build(input_shape=[None, 400, 400, 3])
+model.summary()
+
+# optimizer 梯度优化 1e-3  或者 1e-2
+# loss 
+# metrics  实时查看测试的正确率
+model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
+                loss=tf.losses.CategoricalCrossentropy(),
+                metrics=['accuracy']
+                )
+history = model.fit(train_db, epochs=2, validation_data=test_db, validation_freq=1)
+
+# 再次测试模型
+# model.evaluate(test_db)
+#保存 训练时使用
+model.save_weights('ckpt9-2/weights.ckpt')
+# tf.saved_model.save(model, './model')
+# model.save('my_model')
+# print('saved to my_model')
+
+# reconstructed_model = keras.models.load_model("my_model")
+
+# test_input = tf.ones([1,32,32,3])
+
+# Let's check:
+# print(model.predict(test_input), reconstructed_model.predict(test_input))
+
+
+# print(history)
---
+++ test9.py
@@ -0,0 +1,80 @@
+#  10 个时尚类别的 60,000 个 28x28 灰度图像的数据集，以及一个包含 10,000 个图像的测试集
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
+import matplotlib.pyplot as plt
+
+def load_img(type):
+    train_db = tf.keras.utils.image_dataset_from_directory(
+        directory='/data/tf-python/data/datasets',
+        label_mode='categorical',
+        image_size=(400, 400),
+        batch_size=32,
+        seed=1024,
+        subset=type,
+        validation_split=0.2,
+        follow_links=True)  
+    normalization_layer = layers.Rescaling(1./255)
+    return train_db.map(lambda x, y: (normalization_layer(x), y))
+
+
+#[b,400,400,3]  [b, 5]
+train_db = load_img('training')
+test_db = load_img('validation')
+
+sample = next(iter(test_db))
+print('batch:', sample[0].shape, sample[1].shape)
+
+# plt.figure() 
+
+# for x in range(32):
+   
+#     plt.subplot(x%4+1,x%8+1)
+#     plt.imshow(sample[0][x])
+#     # plt.colorbar()
+#     plt.grid(False) 
+    
+# plt.show()  
+
+baseModel = tf.keras.applications.resnet_v2.ResNet50V2(
+    include_top=False,
+    weights=None,
+    input_tensor=None,
+    input_shape=(400, 400, 3),
+    pooling=None)
+
+model = keras.Sequential([
+        baseModel,
+        layers.Flatten(),
+        keras.layers.Dense(5, activation='softmax')
+    ])    
+
+model.build(input_shape=[None, 400, 400, 3])
+model.summary()
+
+# optimizer 梯度优化 1e-3  或者 1e-2
+# loss 
+# metrics  实时查看测试的正确率
+model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
+                loss=tf.losses.CategoricalCrossentropy(),
+                metrics=['accuracy']
+                )
+history = model.fit(train_db, epochs=2, validation_data=test_db, validation_freq=1)
+
+# 再次测试模型
+# model.evaluate(test_db)
+#保存 训练时使用
+# model.save_weights('ckpt9/weights.ckpt')
+# tf.saved_model.save(model, './model')
+# model.save('my_model')
+# print('saved to my_model')
+
+# reconstructed_model = keras.models.load_model("my_model")
+
+# test_input = tf.ones([1,32,32,3])
+
+# Let's check:
+# print(model.predict(test_input), reconstructed_model.predict(test_input))
+
+
+# print(history)
