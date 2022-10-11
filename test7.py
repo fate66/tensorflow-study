965ecabc112e5211d4d6e7bd2353c585e5f4455c
ft <1610581330@qq.com>
2022-10-11 17:52:17 +0800

    no message

--- test7.py
+++ test7.py
@@ -1,69 +1,136 @@
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import datasets, layers, optimizers, Sequential, metrics
-import keras_tuner as kt
 import json
 import os
 
-def load_img(dir, name):
+
+def load_img(dir, outDir, name):
     image = tf.keras.preprocessing.image.load_img(dir + name)
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     # input_arr = np.array([input_arr])  # Convert single image to a batch.
-    out = tf.image.resize_with_pad(image=input_arr,target_height=400,target_width=400)
-    dirs = path=dir+'resize/'
-    if not os.path.exists(dirs):
-        os.makedirs(dirs)
-    tf.keras.utils.save_img(dirs+name, x=out)
+    out = tf.image.resize_with_pad(
+        image=input_arr, target_height=400, target_width=400)
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+    tf.keras.utils.save_img(outDir+name, x=out)
 
 
-def down(list, dir) :
+def down(list, dir):
     for index, url in enumerate(list):
         print(f"处理第{index}张")
         path_to_downloaded_file = tf.keras.utils.get_file(
             fname=str(index)+'.jpg',
             origin=url,
             cache_subdir=dir,
-            cache_dir='/data/tf-python/data/'
+            cache_dir='/Users/ft/tensorflow-study/data/'
         )
-        load_img('/data/tf-python/data/'+dir+'/', str(index)+'.jpg')
-
+        root = '/Users/ft/tensorflow-study/data/'
+        load_img(root + dir+'/', root + 'datasets/' +
+                 dir + '/', str(index)+'.jpg')
 
 
 # 油画
-def painting8000() :
-    with open("./data/painting8000.json",'r') as load_f:
+def painting8000():
+    with open("./data/painting8000.json", 'r') as load_f:
         load_dict = json.load(load_f)
         down(load_dict, 'paint')
 
 # 瓷器
-def porcelain10000() :
-    with open("./data/porcelain10000.json",'r') as load_f:
+
+
+def porcelain10000():
+    with open("./data/porcelain10000.json", 'r') as load_f:
         load_dict = json.load(load_f)
         down(load_dict, 'porcelain')
 
 
 # 珠宝
-def jewellery5000() :
-    with open("./data/jewellery5000.json",'r') as load_f:
+def jewellery5000():
+    with open("./data/jewellery5000.json", 'r') as load_f:
         load_dict = json.load(load_f)
         down(load_dict, 'jewellery')
 
         # 酒
-def alcohol8000() :
-    with open("./data/alcohol8000.json",'r') as load_f:
+
+
+def alcohol8000():
+    with open("./data/alcohol8000.json", 'r') as load_f:
         load_dict = json.load(load_f)
         down(load_dict, 'alcohol')
 
-        # 玉
-def jade10000() :
-    with open("./data/jade10000.json",'r') as load_f:
+# 玉
+
+
+def jade10000():
+    with open("./data/jade10000.json", 'r') as load_f:
         load_dict = json.load(load_f)
         down(load_dict, 'jade')
-# jade10000()
 
+# 茶叶
+
+
+def tea70_2():
+    with open("./data/tea70_2.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        down(load_dict, 'tea')
+
+# 古籍
+
+
+def ancient10000_2():
+    with open("./data/ancient10000_2.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        down(load_dict, 'ancient')
+
+# 钟表
+
+
+def clocks1300_2():
+    with open("./data/clocks1300_2.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        down(load_dict, 'clocks')
+
+
+# 钱币
+
+
+def coin8000_2():
+    with open("./data/coin8000_2.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        down(load_dict, 'coin')
+
+
+# 钱币
+
+
+def furniture1300_2():
+    with open("./data/furniture1300_2.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        down(load_dict, 'furniture')
+
+        # 钱币
+
+
+def philatelic10000_2():
+    with open("./data/philatelic10000_2.json", 'r') as load_f:
+        load_dict = json.load(load_f)
+        down(load_dict, 'philatelic')
+
+
+# jewellery5000()
+# jade10000()
+# tea70_2()
+# ancient10000_2()
+# clocks1300_2()
+# coin8000_2()
+# furniture1300_2()
+# philatelic10000_2()
+# porcelain10000()
+# painting8000()
+# alcohol8000()
 # print(path_to_downloaded_file)
 
-def test() :
+
+def test():
     train_db = tf.keras.utils.image_dataset_from_directory(
         directory='/data/tf-python/data/datasets',
         label_mode='categorical',
@@ -73,7 +140,7 @@ def test() :
         subset='training',
         validation_split=0.2,
         follow_links=True)
-    
+
     # test_db = tf.keras.utils.image_dataset_from_directory(
     #     directory='/data/tf-python/data/datasets',
     #     label_mode='categorical',
@@ -83,13 +150,67 @@ def test() :
     #     subset='training',
     #     validation_split=0.2,
     #     follow_links=True)
-    
+
     # for (x_train, y_train) in enumerate(train_db):
     #     print(tf.is_tensor(x_train))
-        # print('datasets:', x_train.shape, tf.reduce_max(x_train), tf.reduce_min(x_train))
+    # print('datasets:', x_train.shape, tf.reduce_max(x_train), tf.reduce_min(x_train))
     normalization_layer = layers.Rescaling(1./255)
     train_db = train_db.map(lambda x, y: (normalization_layer(x), y))
     sample = next(iter(train_db))
-    print('batch:', sample[0].shape, sample[1].shape, tf.reduce_max(sample[0]), tf.reduce_min(sample[0]))
+    print('batch:', sample[0].shape, sample[1].shape,
+          tf.reduce_max(sample[0]), tf.reduce_min(sample[0]))
     # print('batch:', sample[0], sample[1])
-test()
+# test()
+
+
+def check(name):
+    if name == '.DS_Store':
+        return False
+    return True
+
+# 判断两个tensor的值是否相等
+
+
+def tensor_equal(a, b):
+    # 判断形状相等
+    if a.shape != b.shape:
+        return False
+    # 逐值对比后若有False则不相等
+    if not tf.reduce_min(tf.cast(a == b, dtype=tf.int32)):
+        return False
+    return True
+
+
+def sortEqual():
+    #  tea
+    root = 'data/datasets/tea/'
+    files = []
+    for fname in os.listdir(root):
+        if check(fname):
+            image = tf.keras.preprocessing.image.load_img(
+                root + fname)
+            input_arr = tf.keras.preprocessing.image.img_to_array(image)
+            files.append({'name': fname, 'arr': input_arr})
+    temp = []
+    print('文件加载完成')
+    for index, obj in enumerate(files):
+        flag = False
+        print('文件开始对比' + str(index))
+        for index, tem in enumerate(temp):
+            # print(obj['arr'])
+            if tensor_equal(obj['arr'], tem[0]['arr']):
+                tem.append(obj)
+                flag = True
+                break
+        if flag == False:
+            temp.append([obj])
+    print('文件对比完成')
+    for index, tem in enumerate(temp):
+        if len(tem) > 1:
+            s = ''
+            for index, sub in enumerate(tem):
+                s += (sub['name'] + '--')
+            print(s)
+
+
+sortEqual()
--- test8_2.py
+++ test8_2.py
@@ -1,8 +1,10 @@
 #  10 个时尚类别的 60,000 个 28x28 灰度图像的数据集，以及一个包含 10,000 个图像的测试集
 import tensorflow as tf
+from tensorflow import keras
 import os
 import time
 # import tensorflowjs as tfjs
+import keras_tuner as kt
 
 tf.random.set_seed(2345)
 
@@ -42,24 +44,22 @@ def create_img(dir, name):
     tf.keras.utils.save_img(dirs+name, x=out)
 
 
-def defModel():
-    #[b,400,400,3]  [b, 5]
-    train_db = load_img('training')
-    test_db = load_img('validation')
-
-    sample = next(iter(test_db))
-    print('batch:', sample[0].shape, sample[1].shape)
-    # print('batch_val', sample[0], sample[1])
+def defModel(hp):
+    hp_units = hp.Int('units', min_value=32, max_value=64, step=32)
+    num_filters_top_layer = hp.Choice(
+        'num_filters_top_layer', values=[10, 64], default=10)
+    filters_layer_1 = hp.Choice(
+        'filters_layer_1', values=[64, 128], default=64)
 
     conv_layers = [
         # accuracy: 0.9979 - val_accuracy: 0.9183
         # layers.Conv2D(64, kernel_size=3, strides=2, padding="same", activation=tf.nn.relu),
         # layers.Conv2D(128, kernel_size=3, strides=2,padding="same", activation=tf.nn.relu),
         # layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
-        tf.keras.layers.Conv2D(64, kernel_size=3, strides=2, kernel_regularizer=tf.keras.regularizers.l2(
+        tf.keras.layers.Conv2D(num_filters_top_layer, kernel_size=3, strides=2, kernel_regularizer=tf.keras.regularizers.l2(
         ), padding="same", activation=tf.nn.relu),
         # tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Conv2D(128, kernel_size=3, strides=2, kernel_regularizer=tf.keras.regularizers.l2(
+        tf.keras.layers.Conv2D(filters_layer_1, kernel_size=3, strides=2, kernel_regularizer=tf.keras.regularizers.l2(
         ), padding="same", activation=tf.nn.relu),
         tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
         tf.keras.layers.Dropout(0.5),
@@ -78,7 +78,7 @@ def defModel():
         # layers.MaxPool2D(pool_size=2, strides=5, padding='same'),
         # 摊平
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(64, activation=tf.nn.relu),
+        tf.keras.layers.Dense(units=hp_units, activation=tf.nn.relu),
         # tf.keras.layers.Dropout(0.5),
         # 最后一个节点的输出必须和分类一致
         # 逻辑回归
@@ -88,25 +88,56 @@ def defModel():
     model.build(input_shape=[None, 400, 400, 3])
     model.summary()
 
-    # optimizer 梯度优化 1e-3  或者 1e-2
-    # loss
-    # metrics  实时查看测试的正确率
-    model.compile(optimizer=tf.keras.optimizers.Adam(1e-4),
+    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
                   loss=tf.losses.CategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy']
                   )
 
-    model.load_weights('ckpt8/weights.ckpt')
+    # model.load_weights('ckpt8/weights.ckpt')
+    return model
 
-    history = model.fit(train_db, epochs=2,
+
+def start():
+    #[b,400,400,3]  [b, 5]
+    train_db = load_img('training')
+    test_db = load_img('validation')
+
+    sample = next(iter(test_db))
+    print('batch:', sample[0].shape, sample[1].shape)
+    # print('batch_val', sample[0], sample[1])
+    tuner = kt.Hyperband(defModel,
+                         objective='val_accuracy',
+                         max_epochs=10,
+                         factor=3,
+                         directory='my_dir',
+                         project_name='intro_to_kt')
+
+    stop_early = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss', patience=5)
+    tuner.search(train_db, epochs=5, validation_data=test_db,
+                 validation_freq=1, callbacks=[stop_early])
+    best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+#  history = model.fit(train_db, epochs=500, validation_data=test_db, validation_freq=1)
+
+    print('units:', best_hps.get('units'),
+          'learning_rate:', best_hps.get('learning_rate'))
+    model = tuner.hypermodel.build(best_hps)
+    # model.load_weights('ckpt6/weights.ckpt')
+
+    history = model.fit(train_db, epochs=5,
                         validation_data=test_db, validation_freq=1)
 
+    val_acc_per_epoch = history.history['val_accuracy']
+    best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+    print('Best epoch: %d' % (best_epoch,))
+
     # 再次测试模型
-    model.evaluate(test_db)
+    # model.evaluate(test_db)
     # 保存 训练时使用
     model.save_weights('ckpt8/weights.ckpt')
-    tf.saved_model.save(model, "model8")
-    # model.save('model8')
+    model.save('model8')
     # tfjs.converters.save_keras_model(model, 'model8js')
 
     # print('saved to my_model')
@@ -133,8 +164,8 @@ def predict():
     image = tf.expand_dims(image, axis=0)
     print('image:', image.shape, tf.reduce_max(image), tf.reduce_min(image))
     start = time.time()
-    model = tf.saved_model.load("model8")
-    print(model(image))
+    model = keras.models.load_model("model8")
+    print(model.predict(image))
     print(time.time() - start)
 
 
@@ -142,5 +173,5 @@ def predict():
 #     create_img('/data/tf-python/data/test/', name)
 
 
-defModel()
+start()
 # predict()
